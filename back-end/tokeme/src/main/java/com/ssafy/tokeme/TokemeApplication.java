fd0505e18a96e9b670f441237fd024b2192ea44a
jiyoung-Heo <hgyoung06@gmail.com>
2024-09-11 12:36:52 +0900

    initial commit

---
+++ back-end/tokeme/src/main/java/com/ssafy/tokeme/TokemeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tokeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokemeApplication.class, args);
+	}
+
+}
---
+++ back-end/tokeme/src/test/java/com/ssafy/tokeme/TokemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tokeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
