599455fbf6cc514402b1ef703b123ee670591d84
jiyoung-Heo <hgyoung06@gmail.com>
2024-10-01 17:22:39 +0900

    feat: Add axios
    axios 초안 올립니다.

--- front-end/tokime/src/api/userInfoAxios.ts
+++ front-end/tokime/src/api/userInfoAxios.ts
@@ -1,9 +1,9 @@
 import API from '../utils/API';
 
-export default async function logoutAxios() {
-  return API.get('/logout')
-    .then(() => {
-      return 'success';
+export default async function userInfoAxios() {
+  return API.get('/user')
+    .then((res) => {
+      return res.data[0];
     })
     .catch((e) => {
       console.log(e);
