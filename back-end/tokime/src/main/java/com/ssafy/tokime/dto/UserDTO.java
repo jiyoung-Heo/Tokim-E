21e9af54ead9aef512ee6a503346981512173e2f
seokjr <116258012+seokjr@users.noreply.github.com>
2024-10-01 18:07:18 +0900

    fix : Fix User value Return
    유저값 리턴하는 거에 birth도 리턴합니다.

--- back-end/tokime/src/main/java/com/ssafy/tokime/dto/UserDTO.java
+++ back-end/tokime/src/main/java/com/ssafy/tokime/dto/UserDTO.java
@@ -5,18 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
     private String name;
     private String email;
+    private Date birth;
     private int phone;
     private Long quizScore;
 
     public UserDTO(final User entity) {
         this.name = entity.getName();
         this.email = entity.getEmail();
+        this.birth = entity.getBirth();
         this.phone = entity.getPhone();
         this.quizScore = entity.getQuizScore();
     }
@@ -25,6 +29,7 @@ public class UserDTO {
         return User.builder()
                 .name(dto.getName())
                 .email(dto.getEmail())
+                .birth(dto.getBirth())
                 .phone(dto.getPhone())
                 .quizScore(dto.getQuizScore())
                 .build();
