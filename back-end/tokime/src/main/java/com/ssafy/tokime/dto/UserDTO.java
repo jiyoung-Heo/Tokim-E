0563ec445661c641abf33522e46a746fda9ab174
jiyoung-Heo <hgyoung06@gmail.com>
2024-10-02 15:27:25 +0900

    fix: update user modify birth, quizscore
    userDTO string으로 받으니까 dto도 그냥 string형으로 변경했어요
    quizscore modify 부분 request body -> param 변경했습니다

--- back-end/tokime/src/main/java/com/ssafy/tokime/dto/UserDTO.java
+++ back-end/tokime/src/main/java/com/ssafy/tokime/dto/UserDTO.java
@@ -5,31 +5,34 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy");
     private String name;
     private String email;
-    private Date birth;
+    private String birth;
     private int phone;
     private Long quizScore;
 
     public UserDTO(final User entity) {
         this.name = entity.getName();
         this.email = entity.getEmail();
-        this.birth = entity.getBirth();
+        this.birth = format.format(entity.getBirth());
         this.phone = entity.getPhone();
         this.quizScore = entity.getQuizScore();
     }
 
-    public static User toEntity(final UserDTO dto) {
+    public static User toEntity(UserDTO dto) throws ParseException {
         return User.builder()
                 .name(dto.getName())
                 .email(dto.getEmail())
-                .birth(dto.getBirth())
+                .birth(format.parse(dto.getBirth()))
                 .phone(dto.getPhone())
                 .quizScore(dto.getQuizScore())
                 .build();
