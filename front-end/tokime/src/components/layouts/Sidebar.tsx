3d60296287d1324b6468d4f761b4c69fdf845bdd
jw17111 <jw171111@gmail.com>
2024-10-02 15:41:00 +0900

    fix:css수정

--- front-end/tokime/src/components/layouts/Sidebar.tsx
+++ front-end/tokime/src/components/layouts/Sidebar.tsx
@@ -27,16 +27,72 @@ const SidebarContainer = styled.div<{ $isOpen: boolean }>`
 
 // 사이드바 아이콘 정의
 const SidebarIcon = styled.img<{ $isOpen: boolean }>`
-  position: fixed;
-  top: 7.34vh;
-  right: 5.56vw;
+  position: relative;
+  top: 5vh;
+  left: 90vw;
   width: 8vw;
   height: auto;
   cursor: pointer;
   z-index: 2000;
-  display: ${(props) => (props.$isOpen ? 'none' : 'block')};
+  visibility: ${(props) =>
+    props.$isOpen ? 'hidden' : 'visible'}; /* visibility로 가리기 */
 `;
 
+// 로고 스타일 정의
+const Logo = styled.img`
+  display: block;
+  margin: 0 auto 3vh auto;
+  width: 30vw;
+`;
+
+// 점수와 백분위
+const GaugeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2vh;
+`;
+
+// 점수 텍스트 스타일
+const Score = styled.p`
+  font-size: 5vw;
+  font-weight: bold;
+`;
+
+// 백분위 텍스트 스타일
+const Percentile = styled.p`
+  font-size: 2.5vw;
+  color: #333333;
+`;
+
+// 사용자 정보 섹션
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3vh;
+`;
+
+const UserName = styled.p`
+  font-size: 3.5vw;
+  margin-bottom: 1.5vh;
+`;
+
+const Icon = styled.img`
+  width: 7vw;
+  height: auto;
+  margin-right: 1vw;
+  cursor: pointer;
+`;
+
+const Divider = styled.hr`
+  width: 90%;
+  border: none;
+  border-top: 1px solid #000000;
+  margin: 1.5vh 0;
+`;
+
+// 버튼 스타일 정의
 const Button = styled.button<{ $bgColor: string; $boxShadow?: string }>`
   width: 80%;
   margin: 2vh auto;
@@ -54,25 +110,57 @@ const Button = styled.button<{ $bgColor: string; $boxShadow?: string }>`
   box-shadow: ${(props) => props.$boxShadow || 'none'};
 `;
 
+const ButtonText = styled.span`
+  margin-left: 2vw;
+  color: #000000;
+`;
+
 function Sidebar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const sidebarRef = useRef<HTMLDivElement>(null);
+  const score = isLoggedIn ? 50 : 0; // 로그인 여부에 따라 점수 설정
+  const navigate = useNavigate();
 
+  const kakaoLoginUrl = `${process.env.REACT_APP_CUSTOM_KEY}/oauth2/authorization/kakao`;
+  const googleLoginUrl = `${process.env.REACT_APP_CUSTOM_KEY}/oauth2/authorization/google`;
+
+  const handleKakaoLogin = () => {
+    window.location.href = kakaoLoginUrl;
+  };
+
+  const handleGoogleLogin = () => {
+    window.location.href = googleLoginUrl;
+  };
+
+  // 사이드바 토글 함수
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
-  // 사이드바가 열리면 스크롤을 비활성화하고, 닫히면 다시 활성화하는 useEffect
-  useEffect(() => {
-    if (isSidebarOpen) {
-      document.body.style.overflow = 'hidden'; // 스크롤 비활성화
-    } else {
-      document.body.style.overflow = 'auto'; // 스크롤 다시 활성화
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      sidebarRef.current &&
+      !sidebarRef.current.contains(event.target as Node)
+    ) {
+      setIsSidebarOpen(false);
     }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
     return () => {
-      document.body.style.overflow = 'auto'; // 컴포넌트가 언마운트될 때 스크롤 다시 활성화
+      document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, [isSidebarOpen]);
+  }, []);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const handleNavigateToLogin = () => {
+    navigate('/login-required'); // LoginRequiredPage로 이동
+  };
 
   return (
     <>
@@ -83,7 +171,73 @@ function Sidebar() {
         $isOpen={isSidebarOpen}
       />
       <SidebarContainer ref={sidebarRef} $isOpen={isSidebarOpen}>
-        {/* 사이드바 내용 */}
+        <Logo src={TokimLogo} alt="Tokim Logo" />
+        {isLoggedIn ? (
+          <>
+            <GaugeWrapper>
+              <Graph score={score} />
+              <Score>{score}점</Score>
+              <Percentile>상위 XX%</Percentile>
+            </GaugeWrapper>
+
+            <UserInfo>
+              <Icon src={sidebarUser} alt="유저 아이콘" />
+              <UserName>홍길동</UserName>
+              <Divider />
+              <Icon src={sidebarEmail} alt="이메일 아이콘" />
+              <p>abcd@kakao.com</p>
+              <Divider />
+              <Icon src={sidebarPhone} alt="전화번호 아이콘" />
+              <p>010-1234-1234</p>
+              <Divider />
+            </UserInfo>
+
+            <Button $bgColor="#00C99C" onClick={() => navigate('/my-page')}>
+              마이페이지
+            </Button>
+            <Button $bgColor="#00C99C" onClick={handleLogout}>
+              로그아웃
+            </Button>
+          </>
+        ) : (
+          <>
+            <GaugeWrapper>
+              <Graph score={score} />
+              <Icon
+                src={sidebarUser}
+                alt="유저 아이콘"
+                onClick={handleNavigateToLogin}
+              />
+              <UserName onClick={handleNavigateToLogin}>
+                로그인 해주세요
+              </UserName>
+            </GaugeWrapper>
+
+            <UserInfo>
+              <Divider />
+              <Icon src={sidebarEmail} alt="이메일 아이콘" />
+              <p>e-mail</p>
+              <Divider />
+              <Icon src={sidebarPhone} alt="전화번호 아이콘" />
+              <p>010-xxxx-xxxx</p>
+              <Divider />
+            </UserInfo>
+
+            <Button $bgColor="#FEE500" onClick={handleKakaoLogin}>
+              <Icon src={KakaoIcon} alt="카카오 아이콘" />
+              <ButtonText>카카오로 로그인</ButtonText>
+            </Button>
+
+            <Button
+              $bgColor="#FFFFFF"
+              $boxShadow="0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+              onClick={handleGoogleLogin}
+            >
+              <Icon src={GoogleIcon} alt="구글 아이콘" />
+              <ButtonText>구글로 로그인</ButtonText>
+            </Button>
+          </>
+        )}
       </SidebarContainer>
     </>
   );
