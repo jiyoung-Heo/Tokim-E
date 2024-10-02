b81175761db24ba4783ec049d90cc0b34ff9a430
jiyoung-Heo <hgyoung06@gmail.com>
2024-10-02 13:04:16 +0900

    fix: Change axios return data
    axios 리턴 데이터 변경하였습니다.

--- front-end/tokime/src/api/userInfoAxios.ts
+++ front-end/tokime/src/api/userInfoAxios.ts
@@ -3,7 +3,7 @@ import API from '../utils/API';
 export default async function userInfoAxios() {
   return API.get('/user')
     .then((res) => {
-      return res.data[0];
+      return res.data.data[0];
     })
     .catch((e) => {
       console.log(e);
