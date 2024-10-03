514333f0c44a862b11bd8b6547912a7ac2d429fc
김선옥 <kso8760@daum.net>
2024-10-03 16:29:46 +0900

    {FIX}#null 용어 즐겨찾기 부분 에러처리

--- back-end/tokime/src/main/java/com/ssafy/tokime/controller/WordController.java
+++ back-end/tokime/src/main/java/com/ssafy/tokime/controller/WordController.java
@@ -61,8 +61,6 @@ public class WordController {
             if (result.isEmpty()) {
                 return ResponseEntity.notFound().build();
             }
-            // 특정 검색 리스트에서도 즐겨찾기한 녀석들이 있을거임, 확인
-            for (int i = 0; i < result.size(); i++) {}
         }
         // Entity -> DTO 변환
         for (Landterm word : result) {
@@ -71,36 +69,41 @@ public class WordController {
         if (SecurityContextHolder.getContext().getAuthentication().getName().equals("anonymousUser")) {
             return ResponseEntity.ok().body(words);
         } else {
-            getUserId();
-            logger.info("로그인 한 상태임!"+user.getEmail());
-            List<LandtermDTO> asdf = (List<LandtermDTO>) getWordLike().getBody();
-            // 가져온 것들이 즐겨찾기에 등록된 것들인지 표시정돈 해주기
-            if (keyword.length() == 0) { // 전체 조회일시 바로 index값으로 접근하면됨
-                for (int i = 0; i < asdf.size(); i++) {
-                    long index = asdf.get(i).getTermId()-1;
-                    words.get((int) index).setLikeCheck(true);
-                }
-            } else { // 특정 키워드로 검색해서 가져온 것들
-                // 키워드 목록과 즐찾목록 비교해서 일치하면 likecheck = ture로 바꿔주기
-                for (int x = 0; x < asdf.size(); x++) {
-                    long number = asdf.get(x).getTermId();
-                    for (int y = 0; y < words.size(); y++) {
-                        if (number == words.get(y).getTermId()) {
-                            // 즐찾 termId와 검색결과들의 termId 비교
-                            words.get(y).setLikeCheck(true);
-                            break;
+            try {
+                getUserId();
+                logger.info("로그인 한 상태임!"+user.getEmail());
+                List<LandtermDTO> asdf = (List<LandtermDTO>) getWordLike().getBody();
+                // 가져온 것들이 즐겨찾기에 등록된 것들인지 표시정돈 해주기
+                if (keyword.length() == 0) { // 전체 조회일시 바로 index값으로 접근하면됨
+                    for (int i = 0; i < asdf.size(); i++) {
+                        long index = asdf.get(i).getTermId()-1;
+                        words.get((int) index).setLikeCheck(true);
+                    }
+                } else { // 특정 키워드로 검색해서 가져온 것들
+                    // 키워드 목록과 즐찾목록 비교해서 일치하면 likecheck = ture로 바꿔주기
+                    for (int x = 0; x < asdf.size(); x++) {
+                        long number = asdf.get(x).getTermId();
+                        for (int y = 0; y < words.size(); y++) {
+                            if (number == words.get(y).getTermId()) {
+                                // 즐찾 termId와 검색결과들의 termId 비교
+                                words.get(y).setLikeCheck(true);
+                                break;
+                            }
                         }
                     }
                 }
+                return ResponseEntity.ok().body(words);
+            }catch (Exception e) {
+                e.printStackTrace();
+                return ResponseEntity.status(500).build();
             }
-            return ResponseEntity.ok().body(words);
         }
     }
 
     // 특정 단어의 상세 내용을 가져오는 메서드
     // Entity -> DTO 변환작업 필요함
     @GetMapping("/{wordId}")
-    public ResponseEntity<?> getWord(@PathVariable long wordId) {
+    public ResponseEntity<?> getWord(@PathVariable("wordId") long wordId) {
         Optional<Landterm> word = wordService.getWord(wordId);
         if (word.isPresent()) {
             LandtermDTO result = new LandtermDTO(word.get());
@@ -116,28 +119,27 @@ public class WordController {
     // 즐겨찾기 조회 메서드
     @GetMapping("/like")
     public ResponseEntity<?> getWordLike() {
+        try {
+            long userId = getUserId();
 
-        // 추후 JWT을 디코딩해서 유저 id를 가져올 예정
-        if (SecurityContextHolder.getContext().getAuthentication().getName() == null) {
-            return ResponseEntity.noContent().build();
-        }
-        long userId = getUserId(); // 여기를 JWT에서 가져옴, 추후 static 으로 뺄 예정
-
-        List<Likeword> words = wordService.getLikeWordList(userId);
-        logger.info("찾은 단어의 갯수 : "+words.size());
-        List<LandtermDTO> result = new ArrayList<>();
-        if (! words.isEmpty()) {
-            // 여기까지 오면 즐겨찾기한 단어들의 id만 갖고있는 상태
-            // 이걸로 모든 단어들을 다시 가져와야함
-            for (Likeword word : words) {
-                Optional<Landterm> landterm = wordService.getWord(word.getTermId());
-                LandtermDTO temp = new LandtermDTO(landterm.get());
-                temp.setLikeCheck(true);
-                result.add(temp);
+            List<Likeword> words = wordService.getLikeWordList(userId);
+            logger.info("찾은 단어의 갯수 : "+words.size());
+            List<LandtermDTO> result = new ArrayList<>();
+            if (! words.isEmpty()) {
+                // 여기까지 오면 즐겨찾기한 단어들의 id만 갖고있는 상태
+                // 이걸로 모든 단어들을 다시 가져와야함
+                for (Likeword word : words) {
+                    Optional<Landterm> landterm = wordService.getWord(word.getTermId());
+                    LandtermDTO temp = new LandtermDTO(landterm.get());
+                    temp.setLikeCheck(true);
+                    result.add(temp);
+                }
             }
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(e);
         }
-        return ResponseEntity.ok().body(result);
-
     }
 
     // 즐겨찾기 용어를 등록하는 메서드
@@ -146,37 +148,54 @@ public class WordController {
     @PostMapping("/like/{wordId}")
     public ResponseEntity<?> likeWord(@PathVariable long wordId) {
         // 추후 JWT을 디코딩해서 유저 id를 가져올 예정
-        long userId = getUserId(); // 여기를 JWT에서 가져옴, 추후 static 으로 뺄 예정
+        try {
+            long userId = getUserId(); // 여기를 JWT에서 가져옴, 추후 static 으로 뺄 예정
+            // 즐겨찾기의 PK는 유저의 ID
+            Likeword word = new Likeword();
+            word.setTermId(wordId);
+            word.setUserId(userId);
 
-        // 즐겨찾기의 PK는 유저의 ID
-        Likeword word = new Likeword();
-        word.setTermId(wordId);
-        word.setUserId(userId);
-
-        wordService.saveLikeWord(word);
-        return ResponseEntity.ok().build();
+            wordService.saveLikeWord(word);
+            return ResponseEntity.ok().build();
+        }catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+            return ResponseEntity.status(500).body(e);
+        }
     }
 
     // 즐겨찾기 해제 메서드
 
     @DeleteMapping("/like/{termId}")
     public ResponseEntity<?> deleteWord(@PathVariable long termId) {
-        // 추후 JWT을 디코딩해서 유저 id를 가져올 예정
-        long userId = getUserId(); // 여기를 JWT에서 가져옴, 추후 static 으로 뺄 예정
-        System.out.println("삭제하고자 하는 용어 : "+termId+" 맞나? :"+likeCheck(termId));
-        wordService.deleteLikeWord(userId, termId);
-        return ResponseEntity.ok().build();
+        try {
+            long userId = getUserId();
+            System.out.println("삭제하고자 하는 용어 : "+termId+" 맞나? :"+likeCheck(termId));
+            wordService.deleteLikeWord(userId, termId);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+            return ResponseEntity.status(500).body(e);
+        }
+
     }
 
     // 즐겨찾기한 용어인지 확인하는 메서드
     public boolean likeCheck(long wordId) {
-        List<LandtermDTO> asdf = (List<LandtermDTO>) getWordLike().getBody();
-        for (LandtermDTO word : asdf) {
-            if (word.getTermId() == wordId) {
-                return true;
+        try {
+            List<LandtermDTO> asdf = (List<LandtermDTO>) getWordLike().getBody();
+            for (LandtermDTO word : asdf) {
+                if (word.getTermId() == wordId) {
+                    return true;
+                }
             }
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
-        return false;
+
     }
 
     // 유저 정보 가져오기
