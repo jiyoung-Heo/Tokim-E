ae339173d52bc130a6758b573da0675bbb8b95d2
jiyoung-Heo <hgyoung06@gmail.com>
2024-10-05 21:34:08 +0900

    feat: Add 투자예정지 목록 페이지 레이어, css, axios 등등 전체
    투자예정지 목록 페이지 기능, 피그마디자인 완료했습니다.

--- front-end/tokime/src/pages/InvestmentPage.tsx
+++ front-end/tokime/src/pages/InvestmentPage.tsx
@@ -1,112 +1,481 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { useNavigate, Link } from 'react-router-dom';
+import backIcon from '../assets/images/icon/left-actionable.png';
+import searchIcon from '../assets/images/icon/search.svg';
+import multiply from '../assets/images/icon/Multiply.png'; // x아이콘
+import {
+  getAllInvestLand,
+  getAllInvestLandFilter,
+} from '../api/landInvestAxios';
 
-// 투자 예정지 스타일
-const InvestItem = styled.div`
-  padding: 10px;
-  margin: 10px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  cursor: pointer;
-  &:hover {
-    background-color: #f0f0f0;
-  }
+// 필요한 스타일 정의
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #f3f7fb;
 `;
 
-// 투자 예정지 등록 버튼 스타일
-const RegisterButton = styled.button`
-  position: fixed;
-  bottom: 80px;
-  right: 20px;
-  padding: 10px 20px;
-  background-color: #007bff;
-  color: #fff;
+const Title = styled.h2`
+  margin: 0 0 3vh 0;
+  font-size: 25px;
+  font-weight: bold;
+  font-family: 'KoddiUD OnGothic';
+  color: #333333;
+  display: flex;
+  justify-content: left;
+`;
+
+// 뒤로가기 아이콘 정의
+const BackIcon = styled.img``;
+
+// 가로선
+const Divider = styled.hr`
+  width: 100%;
   border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  z-index: 800; /* z-index 추가 */
+  border-top: 3px solid rgba(121, 121, 130, 0.1);
+  margin: 3vh 0;
 `;
 
-// 검색창 스타일
+// 전체지역 + 검색창
 const SearchContainer = styled.div`
-  padding: 10px;
   background-color: #f3f7fb;
   display: flex;
   align-items: center;
+  justify-content: space-between; /* 좌우로 공간 분배 */
+  margin-bottom: 4vh;
 `;
 
-// 주소 입력 스타일
+// 지역선택드롭다운시작
+const DropdownContainer = styled.div`
+  position: relative;
+  height: 5vh;
+  width: 35%;
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center; /* 가로 가운데 정렬 */
+  align-items: center; /* 세로 가운데 정렬 */
+`;
+
+const DropdownHeader = styled.div`
+  color: #27c384;
+  font-weight: bold;
+  cursor: pointer;
+  display: flex;
+`;
+
+const DropdownListContainer = styled.div`
+  position: absolute;
+  top: calc(100% + 5px);
+  left: 0;
+  width: 100%;
+  z-index: 1;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  max-height: 150px;
+  overflow-y: auto;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+`;
+
+const DropdownList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+const OptGroupLabel = styled.div`
+  font-weight: bold;
+  padding: 10px;
+  color: #27c384;
+  background-color: #f3f7fb;
+`;
+// 지역선택드롭다운
+const ListItem = styled.li`
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #f3f7fb;
+  }
+`;
+
+// 별칭검색상자
+const SearchBox = styled.div`
+  height: 5vh;
+  width: 60%; /* 너비 조정 */
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  padding-left: 19px;
+`;
+// 검색아이콘
+const SearchIcon = styled.img`
+  width: 5vw;
+  height: 5vh;
+`;
+// x아이콘
+const MultiplyIcon = styled.img`
+  width: 8vw;
+  margin-right: 2vw;
+`;
+// 별칭검색 인풋박스
 const SearchInput = styled.input`
-  width: 50%;
-  padding: 10px;
+  border: none;
   font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  outline: none;
-  margin-right: 10px;
+  font-weight: 700;
+  color: #27c384;
+  margin-left: 10px;
+  width: 100%;
+  background: none;
+  font-family: 'KoddiUD OnGothic';
+
+  &::placeholder {
+    color: rgba(39, 195, 132, 0.5);
+  }
+  &:focus {
+    outline: none;
+    color: #27c384;
+  }
+`;
+// 투자예정지내역나올 컨테이너
+const InvestContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0;
+  box-sizing: border-box;
+  max-height: 60vh; /* 스크롤을 적용할 최대 높이 */
+  overflow-y: auto; /* 수직 스크롤 추가 */
+  width: 100%; /* 너비를 100%로 설정 */
+
+  &::-webkit-scrollbar {
+    width: 4px;
+    transition: opacity 0.3s; /* 부드러운 전환 효과 */
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(39, 195, 132, 0.5);
+    border-radius: 10px;
+  }
+`;
+// 하나의 투자예정지 랩
+const InvestWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+// 투자예정지 왼쪽 글 정보
+const Invest = styled(Link)`
+  width: 60vw;
+  display: block;
+  font-weight: 700;
+  color: #333333;
+  text-decoration: none;
+  font-family: 'KoddiUD OnGothic';
+
+  &:hover {
+    color: #27c384;
+  }
 `;
 
-// 지역 선택 스타일
-const RegionSelect = styled.select`
-  padding: 10px;
-  font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+// 별칭
+const NickName = styled.p`
+  margin: 0;
+  margin-bottom: 1vh;
+  font-size: 17px;
 `;
 
+// 주소
+const Address = styled.p`
+  margin: 0;
+  font-size: 16px;
+`;
+
+// 위험정도나타내줄곳
+const WarningScore = styled.p`
+  margin: 0;
+  font-size: 15px;
+`;
+
+// 투자예정지 오른쪽 맵
+const Map = styled.div`
+  width: 30vw;
+  height: 30vw;
+  background-color: yellow;
+  margin-right: 5vw;
+`;
+const RegistContainer = styled.div`
+  position: fixed; /* 화면에 고정 */
+  bottom: 13vh; /* 화면 아래에서 위로 띄움 */
+  right: 5vw; /* 화면 오른쪽에서 왼쪽으로 띄움 */
+`;
+// 투자 예정지 등록 버튼 스타일
+const RegisterButton = styled.button`
+  padding: 1.5vh 5vw;
+  background-color: #27c384;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  font-weight: bold;
+  cursor: pointer;
+  z-index: 800; /* z-index 추가 */
+  margin-top: 3vh;
+  font-size: 15px;
+`;
+const options = [
+  {
+    options: [{ value: '전국', label: '전국' }],
+  },
+  {
+    label: '특별시',
+    options: [
+      { value: '서울시', label: '서울시' },
+      { value: '세종시', label: '세종시' },
+    ],
+  },
+  {
+    label: '광역시',
+    options: [
+      { value: '부산광역시', label: '부산광역시' },
+      { value: '대구광역시', label: '대구광역시' },
+      { value: '인천광역시', label: '인천광역시' },
+      { value: '광주광역시', label: '광주광역시' },
+      { value: '대전광역시', label: '대전광역시' },
+      { value: '울산광역시', label: '울산광역시' },
+    ],
+  },
+  {
+    label: '도',
+    options: [
+      { value: '경기도', label: '경기도' },
+      { value: '강원도', label: '강원도' },
+      { value: '충청북도', label: '충청북도' },
+      { value: '충청남도', label: '충청남도' },
+      { value: '전라북도', label: '전라북도' },
+      { value: '전라남도', label: '전라남도' },
+      { value: '경상북도', label: '경상북도' },
+      { value: '경상남도', label: '경상남도' },
+      { value: '제주특별자치도', label: '제주도' },
+    ],
+  },
+];
 function InvestmentPage() {
   const navigate = useNavigate();
+  const [searchAlias, setSearchAlias] = useState('');
+  const [allInvest, setAllInvest] = useState<any[]>([]);
+  const [filterInvest, setFilterInvest] = useState<any[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedOption, setSelectedOption] = useState<string>('전국');
 
-  // 투자 예정지 아이템 클릭 시 상세 페이지로 이동
-  const handleItemClick = () => {
-    navigate('/investment-detail'); // 투자 예정지 상세 페이지 경로로 이동
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (label: string) => {
+    setSelectedOption(label);
+    setIsOpen(false);
+  };
+  // 초성 추출 함수
+  const getChosung = (word: string) => {
+    const CHOSUNG = [
+      'ㄱ',
+      'ㄲ',
+      'ㄴ',
+      'ㄷ',
+      'ㄸ',
+      'ㄹ',
+      'ㅁ',
+      'ㅂ',
+      'ㅃ',
+      'ㅅ',
+      'ㅆ',
+      'ㅇ',
+      'ㅈ',
+      'ㅉ',
+      'ㅊ',
+      'ㅋ',
+      'ㅌ',
+      'ㅍ',
+      'ㅎ',
+    ];
+
+    // 자모만 입력된 경우 처리
+    if (CHOSUNG.includes(word)) {
+      return word; // 초성 리스트에 포함된 자모 그대로 반환
+    }
+
+    const unicode = word.charCodeAt(0) - 44032; // 한글의 유니코드 시작 값
+    const chosungIndex = Math.floor(unicode / 588); // 초성 계산
+
+    return CHOSUNG[chosungIndex]; // 초성 반환
+  };
+  // 초성만 입력된 경우인지 확인하는 함수
+  const isChosungOnly = (word: string) => {
+    const CHOSUNG = [
+      'ㄱ',
+      'ㄲ',
+      'ㄴ',
+      'ㄷ',
+      'ㄸ',
+      'ㄹ',
+      'ㅁ',
+      'ㅂ',
+      'ㅃ',
+      'ㅅ',
+      'ㅆ',
+      'ㅇ',
+      'ㅈ',
+      'ㅉ',
+      'ㅊ',
+      'ㅋ',
+      'ㅌ',
+      'ㅍ',
+      'ㅎ',
+    ];
+
+    return [...word].every((char) => CHOSUNG.includes(char));
   };
 
   // 등록 버튼 클릭 시 투자 예정지 등록 페이지로 이동
   const handleRegisterClick = () => {
     navigate('/investment-register'); // 투자 예정지 등록 페이지 경로로 이동
   };
+  const goBack = () => {
+    navigate(-1); // 이전 페이지로 이동
+  };
+
+  useEffect(() => {
+    const fetchInvestmentData = async () => {
+      const data = await getAllInvestLand();
+      if (data) {
+        setAllInvest(data);
+      }
+    };
+    fetchInvestmentData();
+  }, []);
+
+  useEffect(() => {
+    const fetchInvestmentFilterData = async () => {
+      if (selectedOption === '전국') {
+        const data = await getAllInvestLand();
+        if (data) {
+          setAllInvest(data);
+        }
+      } else {
+        const data = await getAllInvestLandFilter(selectedOption);
+        if (data) {
+          setAllInvest(data);
+        }
+      }
+    };
+    fetchInvestmentFilterData();
+  }, [selectedOption]);
+
+  useEffect(() => {
+    if (searchAlias === '') {
+      setFilterInvest(allInvest);
+      // 초성만 입력된 검색
+    } else if (isChosungOnly(searchAlias)) {
+      setFilterInvest(
+        allInvest.filter(
+          (invest) =>
+            getChosung(invest.landNickname) === getChosung(searchAlias),
+        ),
+      );
+    } else {
+      // 단어검색
+      setFilterInvest(
+        allInvest.filter((invest) => invest.landNickname.includes(searchAlias)),
+      );
+    }
+  }, [allInvest, searchAlias]);
 
   return (
-    <div>
-      <h1>나의 투자예정지</h1>
+    <Container>
+      <Title>
+        <BackIcon src={backIcon} alt="back Icon" onClick={goBack} />
+        나의 투자 예정지
+      </Title>
       <SearchContainer>
-        <RegionSelect>
-          <option value="">전체 지역</option>
-          <optgroup label="특별시">
-            <option value="서울특별시">서울특별시</option>
-            <option value="세종특별자치시">세종특별자치시</option>
-          </optgroup>
-          <optgroup label="광역시">
-            <option value="부산광역시">부산광역시</option>
-            <option value="대구광역시">대구광역시</option>
-            <option value="인천광역시">인천광역시</option>
-            <option value="광주광역시">광주광역시</option>
-            <option value="대전광역시">대전광역시</option>
-            <option value="울산광역시">울산광역시</option>
-          </optgroup>
-          <optgroup label="도">
-            <option value="경기도">경기도</option>
-            <option value="강원도">강원도</option>
-            <option value="충청북도">충청북도</option>
-            <option value="충청남도">충청남도</option>
-            <option value="전라북도">전라북도</option>
-            <option value="전라남도">전라남도</option>
-            <option value="경상북도">경상북도</option>
-            <option value="경상남도">경상남도</option>
-            <option value="제주특별자치도">제주특별자치도</option>
-          </optgroup>
-        </RegionSelect>
-        <SearchInput type="text" placeholder="주소를 입력하세요." />
+        <DropdownContainer>
+          <DropdownHeader onClick={toggleDropdown}>
+            {selectedOption}
+            <span>{isOpen ? '▲' : '▼'}</span>
+          </DropdownHeader>
+          {isOpen && (
+            <DropdownListContainer>
+              <DropdownList>
+                {options.map((group, index) => (
+                  <React.Fragment key={index}>
+                    <OptGroupLabel>{group.label}</OptGroupLabel>
+                    {group.options.map((option) => (
+                      <ListItem
+                        key={option.value}
+                        onClick={() => handleOptionClick(option.label)}
+                      >
+                        {option.label}
+                      </ListItem>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </DropdownList>
+            </DropdownListContainer>
+          )}
+        </DropdownContainer>
+        <SearchBox>
+          <SearchIcon src={searchIcon} alt="search" />
+          <SearchInput
+            type="text"
+            placeholder="별칭 검색"
+            value={searchAlias}
+            onChange={(e) => setSearchAlias(e.target.value)}
+          />
+          <MultiplyIcon
+            src={multiply}
+            alt="multiply"
+            onClick={() => setSearchAlias('')}
+          />
+        </SearchBox>
       </SearchContainer>
-      <InvestItem onClick={handleItemClick}>
-        서울시 강남구 테헤란로 123 (임시 데이터)
-      </InvestItem>
-      <RegisterButton onClick={handleRegisterClick}>
-        투자 예정지 등록
-      </RegisterButton>
-    </div>
+
+      <InvestContainer>
+        {filterInvest.length > 0 ? (
+          filterInvest.map((invest) => (
+            <>
+              <InvestWrapper key={invest.investmentPlannedLandId}>
+                <Map />
+                <Invest
+                  to={`/investment-detail/${invest.investmentPlannedLandId}`}
+                >
+                  <NickName>[ {invest.landNickname} ]</NickName>
+                  <Address>{invest.landAddress}</Address>
+                  <WarningScore>
+                    위험정도 :{' '}
+                    {invest.landDanger === 1
+                      ? '중간'
+                      : invest.landDanger === 2
+                        ? '높음'
+                        : '낮음'}
+                  </WarningScore>
+                </Invest>
+              </InvestWrapper>
+              <Divider />
+            </>
+          ))
+        ) : (
+          <div>저장한 투자 예정지가 없습니다.</div>
+        )}
+        {/* <Divider /> */}
+      </InvestContainer>
+      <RegistContainer>
+        <RegisterButton onClick={handleRegisterClick}>등록하기</RegisterButton>
+      </RegistContainer>
+    </Container>
   );
 }
 
