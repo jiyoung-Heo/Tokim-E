61931a23f13ff76cbd4eaf444f6b300f07f5e623
seokjr <116258012+seokjr@users.noreply.github.com>
2024-10-05 16:50:08 +0900

    Fix : Add UserRespository isDeleted

--- back-end/tokime/src/main/java/com/ssafy/tokime/repository/UserRepository.java
+++ back-end/tokime/src/main/java/com/ssafy/tokime/repository/UserRepository.java
@@ -12,22 +12,22 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("select u from User u where u.email = :email")
+    @Query("select u from User u where u.email = :email and u.isDeleted = false")
     Optional<User> findByEmail(@Param("email") String email);
 
     // 특정 연령대의 퀴즈점수 모두 가져오기 - 중복 허용 X 내림차순으로
-    @Query("SELECT DISTINCT u.quizScore FROM User u WHERE YEAR(u.birth) BETWEEN :startYear AND :endYear AND u.quizScore >= 0 order by u.quizScore desc")
+    @Query("SELECT DISTINCT u.quizScore FROM User u WHERE YEAR(u.birth) BETWEEN :startYear AND :endYear AND u.quizScore >= 0 AND u.isDeleted = false order by u.quizScore desc")
     List<Long> findQuizScoreByBirthYearBetween(@Param("startYear") int startYear, @Param("endYear") int endYear);
 
     // 모든 연령대 퀴즈점수 모두 가져오기 - 중복 허용
-    @Query("SELECT u.quizScore FROM User u where u.quizScore >= 0 order by u.quizScore desc")
+    @Query("SELECT u.quizScore FROM User u where u.quizScore >= 0 AND u.isDeleted = false order by u.quizScore desc")
     List<Long> findAllByQuizScore();
 
     // 모든 연령대 쥐크점수 모두 가져오기 - 중복 허용 X
-    @Query("SELECT DISTINCT u.quizScore FROM User u where u.quizScore >= 0 order by u.quizScore desc")
+    @Query("SELECT DISTINCT u.quizScore FROM User u where u.quizScore >= 0 AND u.isDeleted = false order by u.quizScore desc")
     List<Long> findAllByQuizScoreDistinct();
 
     //차트에 필요한 모든 사용자의 점수 분포 가져오기
-    @Query("select u.quizScore, count(u.quizScore) from User u where u.quizScore >= 0 group by u.quizScore order by u.quizScore")
+    @Query("select u.quizScore, count(u.quizScore) from User u where u.quizScore >= 0 and u.isDeleted = false group by u.quizScore order by u.quizScore")
     List<Object[]> findAllByScoreList();
 }
