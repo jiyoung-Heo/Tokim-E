0b2fc55d486d21a0c6b98ed2e9a32bc9d22fb9b7
jiyoung-Heo <hgyoung06@gmail.com>
2024-10-07 14:54:41 +0900

    feat: Add es for 판례, 기사
    사연과 유사한 판례, 기사 elasticsearch 호출 후 보이기까지완료입니다

--- front-end/tokime/src/components/Tabs/StoryAdviceTab.tsx
+++ front-end/tokime/src/components/Tabs/StoryAdviceTab.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import elasticAxios from '../../api/elasticAxios';
+import elasticNewsAxios from '../../api/elasticNewsAxios';
+import elasticLawsAxios from '../../api/elasticLawsAxios';
 
 interface StoryAdviceTabProps {
   story: string | null; // story는 null일 수 있기 때문에 null을 허용
 }
+
 // 스타일 정의
 const Container = styled.div`
   width: 100%;
@@ -38,27 +40,79 @@ const TermDescription = styled.div`
   color: #000000;
 `;
 
+const NoNewsMessage = styled(TermDescription)`
+  // color: #ff0000; // 예시로 빨간색
+  text-align: center;
+`;
+
 const StoryAdviceTab: React.FC<StoryAdviceTabProps> = ({ story }) => {
+  const [newsData, setNewsData] = useState<any[]>([]); // newsData 상태 추가
+  const [lawsData, setLawsData] = useState<any[]>([]); // newsData 상태 추가
+
   useEffect(() => {
     // stoty가지고 판례랑 기사 검색해올게용
     const fetchEsNewsData = async () => {
-      const data = await elasticAxios(story);
+      const data = await elasticNewsAxios(story);
       if (data) {
         console.log(data);
-        // setInvestmentInfo(data);
+        // original_data를 배열로 저장
+        const originalDataArray = data.map(
+          // eslint-disable-next-line no-underscore-dangle
+          (item: any) => item._source.original_data,
+        );
+        setNewsData(originalDataArray); // 상태 업데이트
+        console.log(newsData);
+      }
+      const dataLaws = await elasticLawsAxios(story);
+      if (dataLaws) {
+        console.log(dataLaws);
+        // original_data를 배열로 저장
+        const originalDataArray = dataLaws.map(
+          // eslint-disable-next-line no-underscore-dangle
+          (item: any) => item._source.original_data,
+        );
+        setLawsData(originalDataArray); // 상태 업데이트
       }
     };
     fetchEsNewsData();
-  }, []);
+  }, [story]);
   return (
     <Container>
       <RelatedLawsTitle>사연</RelatedLawsTitle>
       <RelatedLawsContainer>
         <TermDescription>{story}</TermDescription>
       </RelatedLawsContainer>
-      <RelatedLawsTitle>위험사례</RelatedLawsTitle>
+      <RelatedLawsTitle>유사 News</RelatedLawsTitle>
       <RelatedLawsContainer>
-        <TermDescription>내용</TermDescription>
+        {newsData.length > 0 ? (
+          newsData.map((item, index) => (
+            <TermDescription key={index}>
+              <a href={item.link}>{item.title}</a>
+            </TermDescription>
+          ))
+        ) : (
+          <NoNewsMessage>유사한 기사가 없습니다.</NoNewsMessage>
+        )}
+      </RelatedLawsContainer>
+      <RelatedLawsTitle>유사 판례</RelatedLawsTitle>
+      <RelatedLawsContainer>
+        <TermDescription>
+          {lawsData.length > 0 ? (
+            lawsData.map((item, index) => (
+              <TermDescription key={index}>
+                {item.사건명}
+                <br />
+                {item.사건번호}
+                <br />
+                {item.사건종류명}
+                <br />
+                {item.판례내용}
+              </TermDescription>
+            ))
+          ) : (
+            <NoNewsMessage>유사한 판례가 없습니다.</NoNewsMessage>
+          )}
+        </TermDescription>
       </RelatedLawsContainer>
     </Container>
   );
