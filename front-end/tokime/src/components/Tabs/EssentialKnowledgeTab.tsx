6fe014a89a5a161484fb0d16c13753f3779854ce
jw17111 <jw171111@gmail.com>
2024-10-08 17:06:37 +0900

    안해찬바보

--- front-end/tokime/src/components/Tabs/EssentialKnowledgeTab.tsx
+++ front-end/tokime/src/components/Tabs/EssentialKnowledgeTab.tsx
@@ -24,7 +24,27 @@ import know18 from '../../assets/images/knowledge/18.png';
 import know19 from '../../assets/images/knowledge/19.png';
 import know20 from '../../assets/images/knowledge/20.png';
 import know21 from '../../assets/images/knowledge/21.png';
-
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
+// 안해찬 바보똥멍청이
 const TabContent = styled.div`
   height: 60vh;
   background-color: #ffffff;
